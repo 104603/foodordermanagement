cc76a877f66a55752cb49341a73fe9036d813ec7
104603 <62369661+104603@users.noreply.github.com>
2020-10-14 19:16:43 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,28 +1,4 @@
 #include <food.h>
-struct employee
-{
-
-	char name[30];
-	int id;
-	char DOB[10];
-	int dd; int mm; int yyyy;
-	int YOJ;
-	char place[20];
-	float salary;
-	char department[20];
-
-	int quantity;
-};
-struct employee e;
-
-FILE *fp,*ft,*dp,*dt;
-
-int s;
-char findrecord;
-int again;
-int quantity;
-double total=0;
-int t;
 
 int main()
 {
