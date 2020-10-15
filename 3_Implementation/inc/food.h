fbedc46631358e0c780484e16d811256e95ee660
104603 <62369661+104603@users.noreply.github.com>
2020-10-16 02:23:02 +0530

    Update food.h

--- 3_Implementation/inc/food.h
+++ 3_Implementation/inc/food.h
@@ -1,8 +1,11 @@
+#ifndef FOOD_H_INCLUDED
+#define FOOD_H_INCLUDED
+
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
 #include<stdlib.h>
-#include<windows.h>
+//#include<windows.h>
 
 void returnfunc(void);
 
@@ -23,28 +26,5 @@ void aboutus(void);
 
 void Password(void);
 int getdata();
-struct employee
-{
-
-	char name[30];
-	int id;
-	char DOB[10];
-	int dd; int mm; int yyyy;
-	int YOJ;
-	char place[20];
-	float salary;
-	char department[20];
-
-	int quantity;
-};
-struct employee e;
-
-FILE *fp,*ft,*dp,*dt;
-
-int s;
-char findrecord;
-int again;
-int quantity;
-double total=0;
-int t;
+#endif 
 
