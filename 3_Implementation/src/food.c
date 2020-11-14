16cd595a36cc697804fcc2ab998abefe3d4477d7
104603 <62369661+104603@users.noreply.github.com>
2020-11-14 16:17:48 +0530

    Update food.c

--- 3_Implementation/src/food.c
+++ 3_Implementation/src/food.c
@@ -25,6 +25,45 @@ int quantity;
 double total=0;
 int t;
 
+void mainmenu(void)
+{
+	system("cls");
+	printf("                **************************************************************************\n");
+	printf("                                    WELCOME TO FOOD ORDER SYSTEM\n");
+	printf("                **************************************************************************\n\n\n");
+	printf("          1. ADMINISTRATION SECTION--> \n\n          2. CUSTOMER SECTION--> \n\n          3. ABOUT US--> \n\n          4. Exit--> \n\n\n             Enter Your Choice --->");
+	int choice;
+	scanf("%d",&choice);
+	if(choice==1)
+	{
+		administration();
+	}
+	else if(choice==2)
+	{
+		customer();
+	}
+	else if(choice==3)
+	{
+		aboutus();
+	}
+	else if(choice==4)
+	{
+		system("cls");
+		printf("\n\n\n");
+		printf("*******************************THANK YOU**************************************\n");
+
+		printf("*********Feeling HUNGRY ? Call or Walk-In FOODIE GOODIE KITCHEN***************\n\n");
+		exit(0);
+	}
+	else
+	{
+		printf("Wrong Input !! PLease Re-enter The Correct Option");
+		if(getch()){
+                system("cls");
+		mainmenu();}
+	}
+}
+
 
 void administration(void)
 {
